73c0d13b93a02694804b8f8f492e606676bc8203
Petr Sumkin <sumkin.gpb@gmail.com>
2024-06-24 11:31:54 +0300

    init

---
+++ src/main/java/org/example/generatingmodels/GeneratingModelsApplication.java
@@ -0,0 +1,13 @@
+package org.example.generatingmodels;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeneratingModelsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GeneratingModelsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/generatingmodels/GeneratingModelsApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.generatingmodels;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeneratingModelsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
